f48285e3c9bb77933476fc331eba53ad5c331d43
npspears <40127181+npspears@users.noreply.github.com>
2021-04-22 15:05:26 -0500

    updated keymap name

--- candybar_ortho/keymaps/2x3u/keymap.c
+++ candybar_ortho/keymaps/2x3u/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Esc    |  OS   |  Alt  |  Layer  |    Space     |  Alt |  RGB  |   Super  |
  * `-----------------------------------------------------------------------------------'
  */
-[0] = LAYOUT_ortho_2x3u( \
+[0] = LAYOUT_2x3u( \
   KC_TAB,        KC_Q,       KC_W,    KC_F,    KC_P,      KC_G,  KC_NLCK,   KC_P7,         KC_P8,   KC_P9,  KC_HOME,    KC_J,    KC_L,    KC_U,     KC_Y,  KC_SCLN, KC_BSPC, \
   CTL_T(KC_ESC), KC_A,       KC_R,    KC_S,    KC_T,      KC_D,   KC_INS,   KC_P4,         KC_P5,   KC_P6,  KC_PGUP,    KC_H,    KC_N,    KC_E,     KC_I,     KC_O, KC_QUOT, \
   KC_LSFT,       KC_Z,       KC_X,    KC_C,    KC_V,      KC_B,   KC_DEL,   KC_P1,         KC_P2,   KC_P3,  KC_PGDN,    KC_K,    KC_M, KC_COMM,   KC_DOT,  KC_SLSH,  KC_ENT, \
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[1] = LAYOUT_ortho_2x3u( \
+[1] = LAYOUT_2x3u( \
   KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______, _______, _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
   KC_SLSH,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, _______, _______, _______, _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSLS, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PPLS,  KC_EQL, _______, \
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[2] = LAYOUT_ortho_2x3u( \
+[2] = LAYOUT_2x3u( \
   RGB_HUI,  RGB_SAI, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, _______, KC_LBRC, KC_RBRC, RGB_VAI, \
   RGB_HUD,  RGB_SAD, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, KC_MINS, _______, KC_LCBR, KC_RCBR, RGB_VAD, \
   _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, \
